c29cf23b03cc05c83a8fdfbe692e5b112d4d07a5
Guilherme-Abraao <abraaosilva.guilherme@gmail.com>
2024-07-20 19:17:52 -0300

    Ambiente de desenvolvimento

---
+++ scripts/nave.gd
@@ -0,0 +1,17 @@
+extends CharacterBody2D
+
+const SPEED = 500.0
+
+func _physics_process(delta):
+	# Get the input direction and handle the movement.
+	var direction = Vector2(
+		Input.get_axis("ui_left", "ui_right"),
+		Input.get_axis("ui_up", "ui_down")
+	)
+
+	if direction != Vector2.ZERO:
+		velocity = direction.normalized() * SPEED
+	else:
+		velocity = Vector2.ZERO
+
+	move_and_slide()
