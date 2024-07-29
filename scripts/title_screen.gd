afcd22912660606fe0526a175e77b1ab6776035f
Matheus Augusto <matheus4medeiros4@gmail.com>
2024-07-29 17:31:00 -0300

    Corrigir tela inicial

--- scripts/title_screen.gd
+++ scripts/title_screen.gd
@@ -12,7 +12,7 @@ func _process(delta):
 
 
 func _on_start_btn_pressed():
-	get_tree().change_scene_to_file("res://sapace.tscn")
+	get_tree().change_scene_to_file("res://scenes/space.tscn")
 
 
 func _on_credits_btn_pressed():
